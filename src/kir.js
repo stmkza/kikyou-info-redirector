7d382a479c32d41aabbc2848ad01ca993f4bac11
stmkza <stmkza@gmail.com>
2019-06-18 20:01:34 +0900

    マッチしなかったときの処理を調整

--- src/kir.js
+++ src/kir.js
@@ -14,14 +14,16 @@ function rewriteUrl(url) {
                 // GitHub APIで対応するコミットを検索し、コミットページに転送する
                 // ここではasync functionが使えないっぽいので、別のページを表示して、そのページからリダイレクトする
                 const changesetId = url.pathname.replace(/^(\/trac\/kirikiri\/changeset\/)/, '');
-                return `https://stmkza.github.io/kikyou-info-redirector/ChangesetToGitHub.html#${changesetId}`;
+                return `https://stmkza.github.io/kikyou-info-redirector/                        ChangesetToGitHub.html#${changesetId}`;
             }
+            return 'https://github.com/krkrz/krkr2';
             break;
         case 'devdoc.kikyou.info':
             if (url.pathname.startsWith('/tvp/docs/')) {
                 // 吉里吉里2 ドキュメント
                 return `https://krkrz.github.io/krkr2doc/${url.pathname.replace(/^(\/tvp\/docs\/)/, '')}`;
             }
+            return 'https://krkrz.github.io/krkr2doc/';
             break;
         case 'kikyou.info':
             if (url.pathname.startsWith('/tvp/')) {
@@ -30,10 +32,10 @@ function rewriteUrl(url) {
                     // もうないっぽいので、吉里吉里Z　Q&A(Googleグループ) に転送する
                     return 'https://groups.google.com/forum/?hl=ja#!forum/krkrzqa';
                 }
+                return 'https://krkrz.github.io/';
             }
+            return undefined;
     }
-    // どれにもマッチしなかったら吉里吉里Zのサイトに転送する
-    return 'https://krkrz.github.io/';
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
